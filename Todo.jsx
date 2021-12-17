599f57356690734b2e0d4827eeb61b2031aaf3ba
SARVESH KUMAR SHARMA <81557711+SARVESHSHARMA-SKS@users.noreply.github.com>
2021-12-17 09:17:50 +0530

    Create Todo.jsx

---
+++ Todo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import { Todo } from "./input/Todo";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <Todo className="sks" />
+      </div>
+    </>
+  );
+}
